6a36cc17513e4653a355277c6d39c2d22e0d223e
ledungcobra <ledungcobra@gmail.com>
2020-08-27 14:39:43 +0700

    Add

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from .models import Blog
 # Create your views here.
 def home(request):
-    return render(request,'blog/home.html')
+    blogs = Blog.objects.order_by('-date')[:5]
+    
+    return render(request,'blog/home.html',{'blogs':blogs})
+
