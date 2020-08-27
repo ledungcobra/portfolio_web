f7e759d0976c806ebbce152cfbaba2eb46e9636a
ledungcobra <ledungcobra@gmail.com>
2020-08-27 18:30:30 +0700

    Update

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from blog import views
+app_name = 'blog'
 urlpatterns = [
-    path('',views.home)
-    
+    path('',views.all_blogs,name='all_blogs'),
+    path('<int:blog_id>/',views.detail,name='detail'),    
 ]
